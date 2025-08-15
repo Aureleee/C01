866b65e0e94c43d553fa9e73acac20a040d9fd93
Aurele Habbard <ahabbard@f0r10s8.clusters.42paris.fr>
2025-08-15 12:33:49 +0200

    ALORS PEUT-ETRE

--- ex07/ft_rev_int_tab.c
+++ ex07/ft_rev_int_tab.c
@@ -6,7 +6,7 @@
 /*   By: ahabbard <ahabbard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/12 10:44:52 by ahabbard          #+#    #+#             */
-/*   Updated: 2025/08/14 21:16:15 by ahabbard         ###   ########.fr       */
+/*   Updated: 2025/08/15 12:30:28 by ahabbard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,30 +25,27 @@ void	ft_rev_int_tab(int *tab, int size)
 	}
 }
 
-/*
-#include <unistd.h>
+// #include <unistd.h>
+// int	main(void)
+// {
+// 	int		tab1[5] = {0, 1, 2, 3, 4};
+// 	int		i;
+// 	char	c;
 
-int	main(void)
-{
-	int		tab1[5] = {0, 1, 2, 3, 4};
-	int		i;
-	char	c;
-
-	i = 0;
-	while (i < 5)
-	{
-		c = tab1[i] + 48;
-		write(1, &c, 1);
-		i++;
-	}
-	write(1, "\n", 1);
-	ft_rev_int_tab(tab1, 5);
-	i = 0;
-	while (i < 5)
-	{
-		c = tab1[i] + 48;
-		write(1, &c, 1);
-		i++;
-	}
-}
-*/
+// 	i = 0;
+// 	while (i < 5)
+// 	{
+// 		c = tab1[i] + 48;
+// 		write(1, &c, 1);
+// 		i++;
+// 	}
+// 	write(1, "\n", 1);
+// 	ft_rev_int_tab(tab1, 5);
+// 	i = 0;
+// 	while (i < 5)
+// 	{
+// 		c = tab1[i] + 48;
+// 		write(1, &c, 1);
+// 		i++;
+// 	}
+// }
